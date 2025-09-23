f753b4ef63183aace7bc39a9ba29f3e2f1d89f3d
Cyn Liu <cynthialiustar@gmail.com>
2025-09-23 13:59:01 -0400

    test create context multiple messages in log w/ layer output

--- vite-project/tests/test_utils.py
+++ vite-project/tests/test_utils.py
@@ -15,3 +15,21 @@ def test_create_context_one():
     testlog = [Message(id=0, message="henlosu")]
     context = create_context(testlog)
     assert context == "User: henlosu\n"
+
+
+def test_create_context_multi():
+    '''
+    create context with multiple messages
+    '''
+    testlog = [
+        Message(id=0, message="henlosu"),
+        Message(id=1, message="Hi there! Can I help you with anything?", layer_outputs={23: "foobar"}),
+        Message(id=2, message="why am I writing this project?"),
+        Message(id=3, message="What project are you referring to? Without seeing the actual file structure and content of your files, I can't provide specific guidance"),
+    ]
+    context = create_context(testlog)
+    answer = """User: henlosu
+Chatbot: Hi there! Can I help you with anything?
+User: why am I writing this project?
+Chatbot: What project are you referring to? Without seeing the actual file structure and content of your files, I can't provide specific guidance\n"""
+    assert context == answer
